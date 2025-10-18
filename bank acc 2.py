38df2e3e1b98c426d63b78dba2de9779c27ba730
Lucky  Naidu <lakshmipapisetty234@gmail.com>
2025-10-18 09:39:26 +0530

    Add files via upload

---
+++ bank acc 2.py
@@ -0,0 +1,26 @@
+# to create the bank account class
+class bank:
+    def __init__(self):
+        self.bal = 0 # initialize balance to 0
+        print("Welcome to the Reserved bank of india")
+        
+    def deposit(self):
+        amt = float(input("Enter amount to be Deposited: "))
+        self.bal += amt
+        print("\nAmount Deposited:", amt)
+    def withdraw(self):
+        amt = float(input("Enter amount to be Withdrawn: "))
+        if self.bal >= amt:
+            self.bal -= amt
+            print("\nYou Withdrew:", amt)
+        else:
+            print("\nInsufficient bal")
+    def display(self):
+        print("\nNet Available Balance =", self.bal)
+        
+# driver code
+if __name__ == "__main__":
+    s = bank()  # Create an object of Bank
+    s.deposit()        # Deposit money
+    s.withdraw()       # Withdraw money
+    s.display()        # Display balance
